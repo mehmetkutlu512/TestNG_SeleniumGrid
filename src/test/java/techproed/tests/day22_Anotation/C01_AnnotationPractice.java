afa3d6ff95bc2d7d6ad5f2008630b48f0cc16ef3
Mehmet Kutlu <mehmetkutlu512@gmail.com>
2023-04-30 21:39:29 +0300

    30.04.2023

---
+++ src/test/java/techproed/tests/day22_Anotation/C01_AnnotationPractice.java
@@ -0,0 +1,21 @@
+package techproed.tests.day22_Anotation;
+
+import org.testng.annotations.*;
+
+public class C01_AnnotationPractice extends C01_Annotation {
+    @Test
+    public void test1() {
+        System.out.println("test1");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("test3");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("test2");
+    }
+
+}
