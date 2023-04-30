b122624c1a7901fac84e2e0c5a4b8df92bc38262
Mehmet Kutlu <mehmetkutlu512@gmail.com>
2023-04-30 18:59:10 +0300

    30.04.2023

--- src/test/java/techproed/tests/day22_Anotation/C03_Priority.java
+++ src/test/java/techproed/tests/day22_Anotation/C03_Priority.java
@@ -9,13 +9,13 @@ import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class C03_Priority {
-
-
-
-
-
-
-
+/*
+        TestNG test method'larını isim sırasına (alfabetik) göre çaliştirir. Eğer isim sıralamasının
+    dışında bir sıralama ile çalışmasını isterseniz o zaman test methodlarına öncelik(priority)
+    tanımlayabiliriz.
+        Priority küçükten büyüğe göre çalışır. Eğer bir test methoduna priority atanmamışsa
+    default olarak priority=0 kabul edilir.
+     */
 
     WebDriver driver;
     @Test(priority = -3)
