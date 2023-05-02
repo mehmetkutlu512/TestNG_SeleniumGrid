df54a6134c02b700510929c4aca578e09cc70fa9
Mehmet Kutlu <mehmetkutlu512@gmail.com>
2023-05-02 22:15:55 +0300

    02.05.2023

---
+++ src/test/java/techproed/utilities/DriverPractice.java
@@ -0,0 +1,53 @@
+package techproed.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
+
+public class DriverPractice {
+
+    private DriverPractice(){
+
+    }
+
+    static WebDriver driver;
+
+    public static WebDriver getDriver (){
+
+        if (driver==null) {
+            switch (ConfigReader.getProperty("browser")){
+
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        }
+
+        return driver;
+    }
+
+    public static void closeDriver (){
+
+        if (driver!=null){
+            driver.close();
+            driver=null;
+        }
+
+    }
+
+
+}
